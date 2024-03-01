54c1548fdb6602a6b74ebce896f13c33c272ba9c
Mateusz Wieczorek <mateusz.wieczorek429@gmail.com>
2024-03-01 22:33:03 +0100

    Adding a view for the product

--- products/views.py
+++ products/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Hoodie
 # Create your views here.
 
 def product_list(request):
     products = Hoodie.objects.all()
     return render(request, 'products/product_list.html', {'products': products})
+
+def product_detail(request, product_id):
+    product = get_object_or_404(Hoodie, pk=product_id)
+    return render(request, 'products/product_detail.html', {'product': product})
