7522f9d45c587924d60ab2c4ffbd79efc94b20ca
Mateusz Wieczorek <mateusz.wieczorek429@gmail.com>
2024-03-02 13:44:00 +0100

    Visual changes and change of the Hoodie model.

--- products/views.py
+++ products/views.py
@@ -6,6 +6,6 @@ def product_list(request):
     products = Hoodie.objects.all()
     return render(request, 'products/product_list.html', {'products': products})
 
-def product_detail(request, product_id):
-    product = get_object_or_404(Hoodie, pk=product_id)
+def product_detail(request, slug):
+    product = get_object_or_404(Hoodie, slug=slug)
     return render(request, 'products/product_detail.html', {'product': product})
