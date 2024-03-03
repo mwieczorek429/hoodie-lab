087c21617883efc9e0e4aecf4793e242abc54093
Mateusz Wieczorek <mateusz.wieczorek429@gmail.com>
2024-03-03 14:20:27 +0100

    Model improvment

--- products/models.py
+++ products/models.py
@@ -1,16 +1,19 @@
 from django.db import models
 
+class Size(models.Model):
+    name = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
+
 class Hoodie(models.Model):
     name = models.CharField(max_length=100)
     slug = models.SlugField(max_length=200)
     details = models.TextField(max_length=300,
                                blank=True)
-    size = models.CharField(max_length=10)
     color = models.CharField(max_length=50)
     price = models.DecimalField(max_digits=10, 
                                 decimal_places=2)
-    avaiable = models.BooleanField(default=True)
-    quantity = models.IntegerField(default=1)
     image = models.ImageField(upload_to='products/',
                               blank=True)
     created = models.DateTimeField(auto_now_add=True)
@@ -21,3 +24,15 @@ class Hoodie(models.Model):
 
     def __str__(self):
         return self.name
+    
+
+class HoodieSize(models.Model):
+    hoodie = models.ForeignKey(Hoodie,
+                               on_delete=models.CASCADE)
+    size = models.ForeignKey(Size,
+                             on_delete=models.CASCADE)
+    stock = models.IntegerField(default=0)   
+    available = models.BooleanField(default=False) 
+
+    def __str__(self):
+        return f'{self.hoodie.name} | Size: {self.size.name} | Stock: {self.stock}'
